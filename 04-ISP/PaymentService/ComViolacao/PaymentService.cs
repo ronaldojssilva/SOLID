94cc3b7a2e9ba6989dbf93623fc8b86db5c08065
Ronaldo Silva <ronaldojssilva@gmail.com>
2025-06-09 10:19:39 -0300

    Interface Segregation Principle (ISP)

---
+++ 04-ISP/PaymentService/ComViolacao/PaymentService.cs
@@ -0,0 +1,60 @@
+﻿namespace PaymentService.ComViolacao;
+
+interface IPaymentService
+{
+    void ProcessCreditCardPaymentAmount(decimal amount);
+
+    void ValidateCreditCard();
+
+    void ProcessLoan(decimal amount);
+}
+
+class CreditCardService : IPaymentService
+{
+    public void ProcessCreditCardPaymentAmount(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ProcessLoan(decimal amount)
+    {
+        //Não deveria existir. não existe empréstimo no cartão
+        throw new NotImplementedException();
+    }
+
+    public void ValidateCreditCard()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+class LoanService : IPaymentService
+{
+    public void ProcessCreditCardPaymentAmount(decimal amount)
+    {
+        //Não deveria existit
+        throw new NotImplementedException();
+    }
+
+    public void ProcessLoan(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ValidateCreditCard()
+    {
+        //Não deveria existir. não existe validação de cartão no empréstimo
+        throw new NotImplementedException();
+    }
+}
+
+/*
+ Explicação da solução:
+
+Neste exemplo, criamos uma interface `IPaymentService` que define três métodos: `ProcessCreditCardPaymentAmount`, `ValidateCreditCard` e `ProcessLoan`. 
+Porém, forçar as classes a implementar todos os métodos viola o princípio da segregação de interface (ISP).
+
+As classes `CreditCardService` e `LoanService` são obrigadas a impleentar métodos.
+ 
+ */
---
+++ 04-ISP/PaymentService/SemViolacao/PaymentService.cs
@@ -0,0 +1,32 @@
+﻿namespace PaymentService.SemViolacao;
+
+interface ICreditCArdPaymentProcessor
+{
+    void ProcessCreditCardPaymentAmount(decimal amount);
+    void ValidateCreditCard();
+}
+
+interface ILoanProcessor
+{
+    void ProcessLoan(decimal amount);
+}
+
+class CreditCardService : ICreditCArdPaymentProcessor
+{
+    public void ProcessCreditCardPaymentAmount(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+    public void ValidateCreditCard()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class LoanService : ILoanProcessor
+{
+    public void ProcessLoan(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
